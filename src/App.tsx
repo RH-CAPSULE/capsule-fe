63966ec7c3ed8d3ebd227c15b8b6bb334b8d9513
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-05 23:15:26 +0900

    fix: snackbar 안 닫히는 버그

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
+import { SnackbarProvider, closeSnackbar } from 'notistack';
 import { QueryClientProvider } from '@tanstack/react-query';
 import Router from './routes';
 import { queryClient } from './apis/queryClient';
 import { AuthProvider } from './auth/AuthProvider';
+import { px } from './utils/styles';
 
 function App() {
   const notistackRef = React.createRef<any>();
-
-  const onClickDismiss = (key: any) => () => {
-    if (!notistackRef?.current) return;
-    notistackRef.current.closeSnackbar(key);
-  };
 
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <SnackbarProvider
           ref={notistackRef}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
           maxSnack={3}
           action={(key) => (
-            <button type="button" onClick={() => onClickDismiss(key)}>
+            <button
+              type="button"
+              style={{
+                all: 'initial',
+                color: '#fff',
+                fontSize: px(12),
+                paddingInline: px(8),
+              }}
+              onClick={() => closeSnackbar(key)}
+            >
               닫기
             </button>
           )}
