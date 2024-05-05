2edd7e47a76e442f73077de711a7cff726fe698d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 01:55:27 +0900

    fix: my-account -> user-info

--- src/apis/path.ts
+++ src/apis/path.ts
@@ -1,5 +1,5 @@
 export const PATH_API = {
   SIGN_IN: '/api/auth/signin',
   SIGN_UP: '/api/auth/signup',
-  MY_ACCOUNT: '/api/account/my-account',
+  USER_INFO: '/api/user',
 };
--- src/apis/queryKeys.ts
+++ src/apis/queryKeys.ts
@@ -1,5 +1,6 @@
 import { PATH_API } from './path';
 
 export const QUERY_KEY = {
-  USER_INFO: PATH_API.MY_ACCOUNT,
+  USER_INFO: PATH_API.USER_INFO,
+  SIGN_IN: PATH_API.SIGN_IN,
 };
