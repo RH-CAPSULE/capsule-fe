261be3efbdf7dc37dd7c5b1060efc5386ad6dda0
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-05 20:38:00 +0900

    feat: splash page

---
+++ src/pages/splash/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const SplashPage = () => (
+  // eslint-disable-next-line no-console
+  <div>SplashPage</div>
+);
+
+export default SplashPage;
