94039b14027a8cc8c4112af0c412061b693f542f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-05 20:36:19 +0900

    style: add eslint disable line

--- src/_mock/TestPage.tsx
+++ src/_mock/TestPage.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import { handleConfirm } from 'react-handle-alert';
 import { API_DOMAIN } from '../static';
 
 const TestPage = () => {
   const test = async () => {
+    const response = await axios.get(`${API_DOMAIN}/test`);
+    console.log(response.data);
+  };
+
+  const login = async () => {
+    if (!(await handleConfirm('로그인 하시겠습니까?'))) return;
     const response = await axios.post(`${API_DOMAIN}/auth/signin`, {
       email: 'ryoon50@gmail.com',
       password: '1234',
@@ -13,6 +20,9 @@ const TestPage = () => {
 
   return (
     <div>
+      <button type="button" onClick={login}>
+        로그인
+      </button>
       <button type="button" onClick={test}>
         api1
       </button>
