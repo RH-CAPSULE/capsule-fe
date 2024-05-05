776b59e6e2bf539a51303da89bc464d940f8d6cf
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-05 23:15:48 +0900

    add: icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as IconClose } from './icon-close.svg';
+export { ReactComponent as IconTwitter } from './icon-x.svg';
+export { ReactComponent as IconGoogle } from './icon-google.svg';
+export { ReactComponent as IconFacebook } from './icon-facebook.svg';
