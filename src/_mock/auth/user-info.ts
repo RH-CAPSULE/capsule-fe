69869c13c94e5c788ef13f0b4bbad3d32de4ff7d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 02:28:12 +0900

    feat: 모킹!!!!!!!!!!!!!

--- src/_mock/auth/user-info.ts
+++ src/_mock/auth/user-info.ts
@@ -3,7 +3,7 @@ import { PATH_API } from 'src/apis/path';
 import { sleep } from '../utils';
 import { API_DOMAIN } from '../../static';
 
-export const signInHandler = http.get(
+export const userInfoHandler = http.get(
   `${API_DOMAIN}${PATH_API.USER_INFO}`,
   async ({ request, params, cookies }) => {
     await sleep(3000);
