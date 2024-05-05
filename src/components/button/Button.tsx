108715e128b9e92f29e53de61b63a1ac3296f795
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-05 23:15:57 +0900

    feat: button props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,10 +4,14 @@ import styles from './styles.module.scss';
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   type?: 'button' | 'submit' | 'reset';
-  // add props
 }
 
-const Button = ({ children, className = '', type = 'button', ...other }: Props) => (
+const Button = ({
+  children,
+  className = '',
+  type = 'button',
+  ...other
+}: Props) => (
   // eslint-disable-next-line react/button-has-type
   <button type={type} className={`${styles.button} ${className}`} {...other}>
     {children}
