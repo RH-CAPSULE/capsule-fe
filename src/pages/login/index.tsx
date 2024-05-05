b990b4e15d9887c890ee8321def58e7e2941d3e5
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 02:30:18 +0900

    feat: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,80 +1,32 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { FormProvider, RHFInput } from 'src/components/hook-form';
-import { Button } from 'src/components/button';
-import { Capsule } from 'src/components/capsule';
-import { useSnackbar } from 'notistack';
-import { IconGoogle } from 'src/assets/icons';
-import { Link } from 'react-router-dom';
-import { PATH } from 'src/routes/path';
+import { Helmet } from 'react-helmet-async';
+// sections
+import {
+  LoginForm,
+  LoginHeader,
+  LoginAuthOption,
+  LoginWithSocial,
+} from 'src/sections/login';
+// styles
 import styles from './styles.module.scss';
 
 /**
  * Login과 회원가입 페이지는 레이아웃을 따로 사용.
  */
 
-const defaultValues = {
-  email: '',
-  password: '',
-};
-
 const LoginPage = () => {
-  const { enqueueSnackbar } = useSnackbar();
-
-  const methods = useForm({
-    defaultValues,
-  });
-
-  const { handleSubmit } = methods;
-
-  const onSubmit = (data: any) => {
-    const { email, password } = data;
-    if (!email) {
-      enqueueSnackbar('이메일을 입력해주세요.', { variant: 'error' });
-      return;
-    }
-    if (!password) {
-      enqueueSnackbar('비밀번호를 입력해주세요.', { variant: 'error' });
-      return;
-    }
-    console.log(data);
-  };
-
   return (
     <div className={styles.layout}>
+      <Helmet>
+        <title> Sign in | Capsule</title>
+      </Helmet>
       <main>
-        <section>
-          <h1>적어줘 타임캡슐!</h1>
-          <Capsule className={styles.capsule} />
-        </section>
-        <section className={styles.formSection}>
-          <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-            <RHFInput name="email" placeholder="이메일" inputMode="email" />
-            <RHFInput type="password" name="password" placeholder="비밀번호" />
+        <LoginHeader />
 
-            <p className={styles.description}>
-              * 비밀번호는 특수문자, 숫자를 포함하여
-              <br />
-              8자리 이상이어야 합니다.
-            </p>
+        <LoginForm />
 
-            <Button type="submit">로그인</Button>
-          </FormProvider>
-        </section>
-        <section>
-          <div className={styles.authOptions}>
-            <Link to={PATH.PASSWORD_INIT}>비밀번호 재설정</Link>
-            <span aria-label="divider" />
-            <Link to={PATH.SIGN_UP}>회원가입</Link>
-          </div>
-          <div className={styles.or}>또는</div>
-        </section>
-        <section>
-          <Button className={styles.socialLoginButton}>
-            <IconGoogle />
-            Sign in with Google
-          </Button>
-        </section>
+        <LoginAuthOption />
+
+        <LoginWithSocial />
       </main>
     </div>
   );
