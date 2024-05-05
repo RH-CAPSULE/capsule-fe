d9c2d52f8f20d932dfaf5e20e2a8f2a3e1ec14f8
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 01:54:48 +0900

    fix: token 분리

--- src/static.ts
+++ src/static.ts
@@ -1,2 +1,3 @@
 export const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
-export const TOKEN_KEY = process.env.REACT_APP_LOCAL_STORAGE_TOKEN_KEY;
+export const ACCESS_TOKEN_KEY = process.env.REACT_APP_ACCESS_TOKEN_KEY;
+export const REFRESH_TOKEN_KEY = process.env.REACT_APP_REFRESH_TOKEN_KEY;
