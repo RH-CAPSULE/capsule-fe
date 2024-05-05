1faec3c0ae517331f3c67c3fb6aa1208b309d014
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 02:31:03 +0900

    feat: login page section 분리

---
+++ src/sections/login/LoginForm.tsx
@@ -0,0 +1,88 @@
+import { FieldErrors, useForm } from 'react-hook-form';
+import { Button } from 'src/components/button';
+import { RHFInput, FormProvider } from 'src/components/hook-form';
+import { useSignIn } from 'src/apis/queries/auth/sign-in';
+import { useSnackbar } from 'notistack';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import sha256 from 'sha256';
+import styles from './styles.module.scss';
+
+interface IFormValues {
+  email: string;
+  password: string;
+}
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('이메일을 입력해주세요.')
+    .matches(/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i, '이메일 형식이 아닙니다.'),
+  password: Yup.string()
+    .required('비밀번호를 입력해주세요.')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+]).{8,}$/,
+      '비밀번호는 특수문자, 숫자를 포함하여 8자리 이상이어야 합니다.'
+    ),
+});
+
+const defaultValues = {
+  email: '',
+  password: '',
+};
+
+const LoginForm = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const signInMutation = useSignIn<IFormValues>();
+
+  const methods = useForm<IFormValues>({
+    defaultValues,
+    resolver: yupResolver(loginSchema),
+  });
+
+  const {
+    handleSubmit,
+    formState: { isValid },
+  } = methods;
+
+  const onSubmit = (data: IFormValues) => {
+    const { email, password } = data;
+    signInMutation.mutate({
+      email,
+      password: sha256(password),
+    });
+  };
+
+  const onInvalid = (error: FieldErrors<IFormValues>) => {
+    if (error.email) {
+      enqueueSnackbar(error.email.message, { variant: 'error' });
+    }
+    if (error.password) {
+      enqueueSnackbar(error.password.message, { variant: 'error' });
+    }
+  };
+
+  return (
+    <section className={styles.section}>
+      <FormProvider
+        methods={methods}
+        onSubmit={handleSubmit(onSubmit, onInvalid)}
+      >
+        <RHFInput name="email" placeholder="이메일" inputMode="email" />
+        <RHFInput type="password" name="password" placeholder="비밀번호" />
+
+        <p className={styles.description}>
+          * 비밀번호는 특수문자, 숫자를 포함하여
+          <br />
+          8자리 이상이어야 합니다.
+        </p>
+
+        <Button type="submit" disabled={!isValid}>
+          로그인
+        </Button>
+      </FormProvider>
+    </section>
+  );
+};
+
+export default LoginForm;
---
+++ src/sections/login/index.ts
@@ -0,0 +1,4 @@
+export { default as LoginForm } from './LoginForm';
+export { default as LoginHeader } from './LoginHeader';
+export { default as LoginWithSocial } from './LoginWithSocial';
+export { default as LoginAuthOption } from './LoginAuthOption';
