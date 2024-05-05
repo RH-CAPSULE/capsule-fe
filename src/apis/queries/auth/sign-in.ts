f21e818d39d997c0c08f6ac945fbf1f21ce37668
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 01:55:43 +0900

    feat: sign-in api

---
+++ src/apis/queries/auth/sign-in.ts
@@ -0,0 +1,30 @@
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+import { useAuthStore } from 'src/store';
+import { useSnackbar } from 'notistack';
+import { setSession } from 'src/auth/utils';
+import { PATH_API } from '../../path';
+import { axiosInstance } from '../../axios';
+import { QUERY_KEY } from '../../queryKeys';
+
+export const useSignIn = <T>(
+  options?: Omit<UseMutationOptions<any, any, T>, 'mutationKey'>
+) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const setIsLoggedIn = useAuthStore((state) => state.setIsLoggedIn);
+
+  return useMutation({
+    mutationKey: [QUERY_KEY.SIGN_IN],
+    mutationFn: async (payload: T) => {
+      const response = await axiosInstance.post(PATH_API.SIGN_IN, payload);
+      return response.data;
+    },
+    onSuccess: (data) => {
+      setSession(data);
+      setIsLoggedIn(true);
+    },
+    onError: (error) => {
+      enqueueSnackbar(error.message, { variant: 'error' });
+    },
+    ...options,
+  });
+};
