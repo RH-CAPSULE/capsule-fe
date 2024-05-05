0a682fd3a9fff3ed748169d71e00539b2638356b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-05 20:37:52 +0900

    style: fix eslint

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,15 +3,15 @@ import styles from './styles.module.scss';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  type?: 'button' | 'submit' | 'reset';
   // add props
 }
 
-const Button = ({ children, className = '', ...other }: Props) => {
-  return (
-    <button className={`${styles.button} ${className}`} {...other}>
-      {children}
-    </button>
-  );
-};
+const Button = ({ children, className = '', type = 'button', ...other }: Props) => (
+  // eslint-disable-next-line react/button-has-type
+  <button type={type} className={`${styles.button} ${className}`} {...other}>
+    {children}
+  </button>
+);
 
 export default Button;
