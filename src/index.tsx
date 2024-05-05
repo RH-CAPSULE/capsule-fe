92a027894bcb91e6872302d0783fd3497e566895
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 01:54:29 +0900

    style: 주석 제거

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,10 @@ const enableMocking = async () => {
 };
 
 enableMocking().then(() => {
-  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
   root.render(<App />);
 });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
