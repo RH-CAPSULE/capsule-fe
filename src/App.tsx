2a0cd5a0e7b9afadc9a349213197381c034a1707
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 01:54:14 +0900

    feat: helmet

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider, closeSnackbar } from 'notistack';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { HelmetProvider } from 'react-helmet-async';
 import Router from './routes';
 import { queryClient } from './apis/queryClient';
 import { AuthProvider } from './auth/AuthProvider';
@@ -13,35 +14,37 @@ function App() {
 
   return (
     <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <SnackbarProvider
-          ref={notistackRef}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-          maxSnack={3}
-          action={(key) => (
-            <button
-              type="button"
-              style={{
-                all: 'initial',
-                color: '#fff',
-                fontSize: px(12),
-                paddingInline: px(8),
-              }}
-              onClick={() => closeSnackbar(key)}
-            >
-              닫기
-            </button>
-          )}
-          autoHideDuration={5000}
-        >
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </SnackbarProvider>
-      </QueryClientProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <SnackbarProvider
+            ref={notistackRef}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            maxSnack={3}
+            action={(key) => (
+              <button
+                type="button"
+                style={{
+                  all: 'initial',
+                  color: '#fff',
+                  fontSize: px(12),
+                  paddingInline: px(8),
+                }}
+                onClick={() => closeSnackbar(key)}
+              >
+                닫기
+              </button>
+            )}
+            autoHideDuration={5000}
+          >
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </SnackbarProvider>
+        </QueryClientProvider>
+      </HelmetProvider>
     </AuthProvider>
   );
 }
