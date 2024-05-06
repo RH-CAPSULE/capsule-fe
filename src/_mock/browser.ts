d8426067874f8432411f1fee7b1db79bd47ea42e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 20:33:41 +0900

    feat: capsuleBox mock api

--- src/_mock/browser.ts
+++ src/_mock/browser.ts
@@ -1,7 +1,8 @@
 import { setupWorker } from 'msw/browser';
 import { signInHandler } from './auth/signin';
 import { userInfoHandler } from './auth/user-info';
+import { capsuleBoxHandler } from './capsule/capsule-box';
 
-const handlers = [signInHandler, userInfoHandler];
+const handlers = [signInHandler, userInfoHandler, capsuleBoxHandler];
 
 export const worker = setupWorker(...handlers);
---
+++ src/_mock/capsule/capsule-box.ts
@@ -0,0 +1,25 @@
+import { http, HttpResponse } from 'msw';
+import { PATH_API } from 'src/apis/path';
+import { sleep } from '../utils';
+import { API_DOMAIN } from '../../static';
+
+export const capsuleBoxHandler = http.get(
+  `${API_DOMAIN}${PATH_API.CAPSULE_BOX}`,
+  async ({ request, params, cookies }) => {
+    await sleep(3000);
+
+    return HttpResponse.json(
+      {
+        capsuleBoxId: 1,
+        theme: 'PURPLE',
+        openedAt: new Date(2024, 7, 31).toISOString(),
+        closedAt: new Date(2024, 6, 30).toISOString(),
+        capsules: ['#aabbcc', '#ddeeff'],
+      },
+      {
+        status: 200,
+        statusText: 'Mocked status',
+      }
+    );
+  }
+);
