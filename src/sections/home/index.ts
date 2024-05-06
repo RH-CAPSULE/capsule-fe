c28a11706f34ec4d9208f99c83023f9b0c0fd87c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 20:36:54 +0900

    feat: home sections

---
+++ src/sections/home/index.ts
@@ -0,0 +1,3 @@
+export { default as HomeHeader } from './HomeHeader';
+export { default as HomeCapsuleBox } from './HomeCapsuleBox';
+export { default as HomeBottomButtons } from './HomeBottomButtons';
