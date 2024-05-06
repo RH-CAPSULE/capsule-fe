0a3973571d53b38b54ece86c5e3fe87532cfa58f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-06 20:34:16 +0900

    feat: capsule box query api

--- src/apis/queries/capsule/capsule-box.ts
+++ src/apis/queries/capsule/capsule-box.ts
@@ -1,20 +1,25 @@
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import {
+  UseQueryOptions,
+  UseQueryResult,
+  useQuery,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { PATH_API } from '../../path';
 import { axiosInstance } from '../../axios';
 import { QUERY_KEY } from '../../queryKeys';
 
 type Props = Omit<UseQueryOptions, 'queryKey'>;
 
-export const useUserInfo = (params?: Props) => {
+export const useCapsuleBox = <T>(params?: Props) => {
   return useQuery({
-    queryKey: [QUERY_KEY.USER_INFO],
+    queryKey: [QUERY_KEY.CAPSULE_BOX],
     queryFn: async () => {
-      const response = await axiosInstance.get(PATH_API.USER_INFO);
+      const response = await axiosInstance.get(PATH_API.CAPSULE_BOX);
       return response.data;
     },
     // 계속 가지고 있을 거임
     gcTime: Infinity,
     staleTime: Infinity,
     ...params,
-  });
+  }) as UseQueryResult<T, AxiosError>;
 };
--- src/apis/queryKeys.ts
+++ src/apis/queryKeys.ts
@@ -1,6 +1,6 @@
 import { PATH_API } from './path';
 
 export const QUERY_KEY = {
-  USER_INFO: PATH_API.USER_INFO,
   SIGN_IN: PATH_API.SIGN_IN,
+  CAPSULE_BOX: PATH_API.CAPSULE_BOX,
 };
