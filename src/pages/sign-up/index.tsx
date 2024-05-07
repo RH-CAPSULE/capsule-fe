de9999afe12afc4309ec46c1769b5f3a6c552490
minjeoong <orange_mj@naver.com>
2024-05-07 20:24:22 +0900

    feat: signup style init

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+// styles
+import { Container } from 'src/components/container';
+import styles from './styles.module.scss';
 
 const SignUpPage = () => (
   // eslint-disable-next-line no-console
-  <div>SignUpPage</div>
+  <div className={styles.layout}>
+    <Container>
+    
+    SignUpPage
+    </Container>
+  
+  </div>
 );
 export default SignUpPage;
