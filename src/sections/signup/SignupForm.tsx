84f5e20390c38678a3a844e0036a7ed91c0c7fc0
minjeoong <orange_mj@naver.com>
2024-05-07 22:23:26 +0900

    feat: signup 화면구성

--- src/sections/signup/SignupForm.tsx
+++ src/sections/signup/SignupForm.tsx
@@ -11,11 +11,16 @@ import styles from './styles.module.scss';
 // ----------------------------------------------------------------------
 
 interface IFormValues {
+  name : string;
   email: string;
   password: string;
+  passwordConfirm: string;
 }
 
 const signupSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('이름을 입력해주세요.')
+    .matches(/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i, '이름 형식이 아닙니다.'),
   email: Yup.string()
     .required('이메일을 입력해주세요.')
     .matches(/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i, '이메일 형식이 아닙니다.'),
@@ -25,16 +30,21 @@ const signupSchema = Yup.object().shape({
       /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+]).{8,}$/,
       '비밀번호는 특수문자, 숫자를 포함하여 8자리 이상이어야 합니다.'
     ),
+  passwordConfirm: Yup.string()
+    .required('비밀번호 확인을 입력해주세요.')
+    .oneOf([Yup.ref('password'), ''], '비밀번호가 일치하지 않습니다.'),
 });
 
 const defaultValues = {
+  name: '',
   email: '',
   password: '',
+  passwordConfirm: '',
 };
 
 // ----------------------------------------------------------------------
 
-const LoginForm = () => {
+const SignupForm = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const signInMutation = useSignIn<IFormValues>();
@@ -50,14 +60,19 @@ const LoginForm = () => {
   } = methods;
 
   const onSubmit = (data: IFormValues) => {
-    const { email, password } = data;
+    const { name, email, password, passwordConfirm } = data;
     signInMutation.mutate({
+      name,
       email,
       password: sha256(password),
+      passwordConfirm: sha256(password),
     });
   };
 
   const onInvalid = (error: FieldErrors<IFormValues>) => {
+    if (error.name) {
+      enqueueSnackbar(error.name.message, { variant: 'error' });
+    }
     if (error.email) {
       enqueueSnackbar(error.email.message, { variant: 'error' });
     }
@@ -72,8 +87,10 @@ const LoginForm = () => {
         methods={methods}
         onSubmit={handleSubmit(onSubmit, onInvalid)}
       >
-        <RHFInput name="email" placeholder="이메일" inputMode="email" />
-        <RHFInput type="password" name="password" placeholder="비밀번호"/>
+        <RHFInput name="name" placeholder="이름" inputMode="email" autoComplete="username"/>
+        <RHFInput name="email" placeholder="이메일" inputMode="email" autoComplete="useremail"/>
+        <RHFInput type="password" name="password" placeholder="비밀번호" autoComplete="current-password"/>
+        <RHFInput type="password" name="passwordConfirm" placeholder="비밀번호 확인"/>
 
         <p className={styles.description}>
           * 비밀번호는 특수문자, 숫자를 포함하여
@@ -86,11 +103,11 @@ const LoginForm = () => {
           disabled={!isValid}
           loading={signInMutation.isPending}
         >
-          로그인
+          회원가입
         </Button>
       </FormProvider>
     </section>
   );
 };
 
-export default LoginForm;
+export default SignupForm;
