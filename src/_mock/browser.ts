5c44546a4896eff5489f4bd3a122ddb408d897d6
minjeoong <orange_mj@naver.com>
2024-05-08 20:22:44 +0900

    feat: signup mock api 수정

--- src/_mock/browser.ts
+++ src/_mock/browser.ts
@@ -2,7 +2,8 @@ import { setupWorker } from 'msw/browser';
 import { signInHandler } from './auth/signin';
 import { userInfoHandler } from './auth/user-info';
 import { capsuleBoxHandler } from './capsule/capsule-box';
+import {signUpHandler} from "./auth/signup";
 
-const handlers = [signInHandler, userInfoHandler, capsuleBoxHandler];
+const handlers = [signInHandler,signUpHandler, userInfoHandler, capsuleBoxHandler];
 
 export const worker = setupWorker(...handlers);
