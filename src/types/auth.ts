0cf29e868238f873b97ee1e7ca54260b8891b909
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-11 21:57:27 +0900

    feat: email verify purpose type

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -3,11 +3,16 @@
  */
 export interface IUser {
   id: number;
-  name: string;
-  email: string;
+  userName: string;
+  userEmail: string;
 }
 
 /**
  * 이메일 검증 시간 타입
  */
 export type Time = Date | number | null | undefined;
+
+export enum EmailVerifyPurpose {
+  SIGN_UP = 'SIGN_UP',
+  RESET_PASSWORD = 'RESET_PASSWORD',
+}
