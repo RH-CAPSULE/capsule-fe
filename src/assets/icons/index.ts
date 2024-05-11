54f49745a9ba0348d2bac0c0eb16ee4f8600e6f0
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-12 03:07:27 +0900

    feat: add icon arrow-back

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -8,6 +8,7 @@ export { ReactComponent as IconGoogle } from './icon-google.svg';
 export { ReactComponent as IconLogout } from './icon-logout.svg';
 export { ReactComponent as IconHistory } from './icon-history.svg';
 export { ReactComponent as IconFacebook } from './icon-facebook.svg';
+export { ReactComponent as IconArrowBack } from './icon-arrow-back.svg';
 export { ReactComponent as IconCapsuleBox } from './icon-capsule-box.svg';
 export { ReactComponent as IconCapsuleBoxOpen } from './icon-capsule-box-open.svg';
 export { ReactComponent as IconCapsuleBoxBlank } from './icon-capsule-box-blank.svg';
