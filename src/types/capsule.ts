6169a17d512e0fb118402f33c79db82994da9f21
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-12 03:12:49 +0900

    feat: MakingCapsuleStep type

--- src/types/capsule.ts
+++ src/types/capsule.ts
@@ -7,3 +7,10 @@ export interface ICapsuleBox {
   closedAt: string;
   capsules: string[];
 }
+
+export enum MakeCapsuleStep {
+  SelectColor = '캡슐함 색상 고르기',
+  SetClosedDate = '캡슐함 봉인일자 설정',
+  SetOpenedDate = '캡슐함 개봉일자 생성',
+  Confirm = '캡슐함을 만드시겠습니까?',
+}
