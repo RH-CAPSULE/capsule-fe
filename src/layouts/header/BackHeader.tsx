ff9d3cad8dc89dac9e89be75410204e8c7c8226c
minjeoong <orange_mj@naver.com>
2024-05-12 03:42:43 +0900

    feat: write 페이지 레이아웃

--- src/layouts/header/BackHeader.tsx
+++ src/layouts/header/BackHeader.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { IconArrowLeft, IconBell } from 'src/assets/icons';
 import styles from './styles.module.scss';
 import NavMenu from './NavMenu';
 
 const BackHeader = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <header className={styles.backHeader}>
-      <IconArrowLeft />
+      <IconArrowLeft onClick={goBack} />
     </header>
   );
 };
