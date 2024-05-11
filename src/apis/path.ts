ba20325782c31475dc041bef6e63c33eb2fdfbd4
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-11 21:53:37 +0900

    feat: path authenticate email

--- src/apis/path.ts
+++ src/apis/path.ts
@@ -1,6 +1,11 @@
 export const PATH_API = {
+  // auth
   SIGN_IN: '/api/auth/signin',
   SIGN_UP: '/api/auth/signup',
+  SEND_EMAIL: '/api/auth/mail/send',
+  VERIFY_EMAIL: '/api/auth/mail/verify',
+  // user
   USER_INFO: '/api/user',
+  // capsule
   CAPSULE_BOX: '/api/capsule-box',
 };
