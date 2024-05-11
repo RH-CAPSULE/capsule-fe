16afe37faf1a5b5c4f2f39dcdb48cbf307e4fb89
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-12 03:10:15 +0900

    feat: add export CapsuleBoxBlank

--- src/sections/home/index.ts
+++ src/sections/home/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeHeader } from './HomeHeader';
 export { default as HomeCapsuleBox } from './HomeCapsuleBox';
 export { default as HomeBottomButtons } from './HomeBottomButtons';
+export { default as HomeCapsuleBoxBlank } from './HomeCapsuleBoxBlank';
