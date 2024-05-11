66db8fa4892fb67ebb20ab9c3673aac19fb64d7d
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-11 21:53:16 +0900

    feat: auth email mock api

--- src/_mock/browser.ts
+++ src/_mock/browser.ts
@@ -2,8 +2,17 @@ import { setupWorker } from 'msw/browser';
 import { signInHandler } from './auth/signin';
 import { userInfoHandler } from './auth/user-info';
 import { capsuleBoxHandler } from './capsule/capsule-box';
-import {signUpHandler} from "./auth/signup";
+import { signUpHandler } from './auth/signup';
+import { sendEmailHandler } from './auth/send-email';
+import { verifyEmailHandler } from './auth/verify-email';
 
-const handlers = [signInHandler,signUpHandler, userInfoHandler, capsuleBoxHandler];
+const handlers = [
+  signInHandler,
+  signUpHandler,
+  userInfoHandler,
+  capsuleBoxHandler,
+  sendEmailHandler,
+  verifyEmailHandler,
+];
 
 export const worker = setupWorker(...handlers);
