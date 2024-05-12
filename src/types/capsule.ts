21cb1c0242f372fec14a964cb543279cb34f3361
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-12 21:56:57 +0900

    refactor: enum type에서 union type으로 일괄 변경

--- src/types/capsule.ts
+++ src/types/capsule.ts
@@ -1,16 +1,20 @@
-import { Theme } from './theme';
+import { ThemeType } from './theme';
+import { Union } from './union';
 
 export interface ICapsuleBox {
   capsuleBoxId?: number;
-  theme: Theme;
+  theme: ThemeType;
   openedAt: string;
   closedAt: string;
   capsules: string[];
 }
 
-export enum MakeCapsuleStep {
-  SelectColor = '캡슐함 색상 고르기',
-  SetClosedDate = '캡슐함 봉인일자 설정',
-  SetOpenedDate = '캡슐함 개봉일자 생성',
-  Confirm = '캡슐함을 만드시겠습니까?',
-}
+// MakeCapsuleStep Type assertion
+export const MakeCapsuleStep = {
+  SelectColor: '캡슐함 색상 고르기',
+  SetClosedDate: '캡슐함 봉인일자 설정',
+  SetOpenedDate: '캡슐함 개봉일자 생성',
+  Confirm: '캡슐함을 만드시겠습니까?',
+} as const;
+
+export type MakeCapsuleStepType = Union<typeof MakeCapsuleStep>;
