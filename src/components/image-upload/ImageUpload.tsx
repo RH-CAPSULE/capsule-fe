388ecdad24abb7ec601e769ec253e7bd48a55e9e
minjeoong <orange_mj@naver.com>
2024-05-13 02:47:34 +0900

    feat: 이미지 업로드 부분 컴포넌트화

---
+++ src/components/image-upload/ImageUpload.tsx
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { IconClose } from 'src/assets/icons';
+
+import styles from './styles.module.scss';
+
+interface props {
+  fileInputRef: React.RefObject<HTMLInputElement>;
+}
+
+const ImageUpload = ({ fileInputRef }: props) => {
+  const { register } = useFormContext();
+  const [imageURL, setImageURL] = useState<string | null>(null);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files?.[0];
+    if (selectedImage) {
+      const imageUrl = URL.createObjectURL(selectedImage);
+      setImageURL(imageUrl);
+      register('image', { value: selectedImage });
+    }
+  };
+
+  const handleImageDelete = () => {
+    setImageURL(null);
+    register('image', { value: null });
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+  };
+
+  return (
+    <div className={styles.image}>
+      {imageURL && (
+        <>
+          <img src={imageURL} alt="Uploaded" className={styles.uploadedImage} />
+          <IconClose
+            onClick={handleImageDelete}
+            className={styles.removeImage}
+          />
+        </>
+      )}
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={handleImageChange}
+        className={styles.disable}
+      />
+    </div>
+  );
+};
+
+export default ImageUpload;
