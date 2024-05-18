34d3c1b5399ad40590323b21490c37c19847c621
minjeoong <orange_mj@naver.com>
2024-05-19 02:45:30 +0900

    feat: make-capsule query

---
+++ src/apis/queries/capsule/make-capsule.ts
@@ -0,0 +1,23 @@
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+import { useSnackbar } from 'notistack';
+import { QUERY_KEY } from 'src/apis/queryKeys';
+import { PATH_API } from '../../path';
+import { axiosInstance } from '../../axios';
+
+export const useMakeCapsule = <T>(
+  options?: Omit<UseMutationOptions<any, any, T>, 'mutationKey'>
+) => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  return useMutation({
+    mutationKey: [QUERY_KEY.MAKE_CAPSULE],
+    mutationFn: async (payload: T) => {
+      const response = await axiosInstance.post(PATH_API.MAKE_CAPSULE, payload);
+      return response.data;
+    },
+    onError: (error) => {
+      enqueueSnackbar(error?.message, { variant: 'error' });
+    },
+    ...options,
+  });
+};
