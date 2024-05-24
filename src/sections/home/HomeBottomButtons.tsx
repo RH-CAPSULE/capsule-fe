e440aa4028b76c85ba81c3afcb67aea6999429b4
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-24 22:19:33 +0900

    feat: 나에게 캡슐 썼는지 표시

--- src/sections/home/HomeBottomButtons.tsx
+++ src/sections/home/HomeBottomButtons.tsx
@@ -6,6 +6,7 @@ import { useSnackbar } from 'notistack';
 import { copyToClipboard } from 'src/utils/clipboard';
 import { PATH } from 'src/routes/path';
 import { useCachedUser } from 'src/apis/queries/auth/user-info';
+import { isFuture, isPast } from 'date-fns';
 import styles from './styles.module.scss';
 import { ICapsuleBox } from '../../types';
 
@@ -27,25 +28,63 @@ interface Props {
 const HomeBottomButtons = ({ theme, capsuleBox }: Props) => {
   const { enqueueSnackbar } = useSnackbar();
   const { user } = useCachedUser();
-  const { capsuleBoxId } = capsuleBox || {};
+  const { capsuleBoxId, hasMyCapsule, closedAt, openedAt } = capsuleBox || {};
 
   const handleCopy = () => {
     copyToClipboard(`${getDomain()}/${user?.id}`);
     enqueueSnackbar('링크가 복사되었습니다.', { variant: 'success' });
   };
 
-  return (
-    <section className={`${styles.section} ${styles.bottom}`}>
-      <Link to={`${PATH.WRITE}?id=${capsuleBoxId}`}>
-        <Button theme={theme} size="large" full>
-          나에게 캡슐 쓰기
+  const isOpened = openedAt && isPast(new Date(openedAt));
+  const isClosed = closedAt && isPast(new Date(closedAt)) && !isOpened;
+  const isWritable = closedAt && isFuture(new Date(closedAt));
+
+  if (isWritable) {
+    return (
+      <section className={`${styles.section} ${styles.bottom}`}>
+        <Link to={`${PATH.WRITE}?id=${capsuleBoxId}`}>
+          <Button theme={theme} size="large" full disabled={hasMyCapsule}>
+            {hasMyCapsule
+              ? '이미 캡슐함을 작성하셨습니다.'
+              : '나에게 캡슐 쓰기'}
+          </Button>
+        </Link>
+        <Button theme={theme} size="large" full onClick={handleCopy}>
+          공유하기
         </Button>
-      </Link>
-      <Button theme={theme} size="large" full onClick={handleCopy}>
-        공유하기
-      </Button>
-    </section>
-  );
+      </section>
+    );
+  }
+
+  if (isClosed) {
+    return (
+      <section className={`${styles.section} ${styles.bottom}`}>
+        <Button theme={theme} size="large" full disabled>
+          캡슐함 개봉까지 D-
+          {Math.ceil(
+            (new Date(openedAt!).getTime() - Date.now()) / (1000 * 60 * 60 * 24)
+          )}
+        </Button>
+      </section>
+    );
+  }
+
+  if (isOpened) {
+    return (
+      <section className={`${styles.section} ${styles.bottom}`}>
+        <Link to={`${PATH.WRITE}?id=${capsuleBoxId}`}>
+          <Button theme={theme} size="large" full>
+            캡슐함 열기
+          </Button>
+        </Link>
+        <Button theme={theme} size="large" full onClick={handleCopy}>
+          새 캡슐함 만들러 가기
+        </Button>
+      </section>
+    );
+  }
+
+  return null;
 };
 
 export default HomeBottomButtons;
