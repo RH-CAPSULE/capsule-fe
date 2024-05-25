82731856d532adf9949366dd332ee4565e7befa9
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-25 15:39:38 +0900

    feat: access token expired시 refresh 로직 추가

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { API_DOMAIN } from '../static';
+import { ACCESS_TOKEN_KEY, API_DOMAIN, REFRESH_TOKEN_KEY } from '../static';
+import { PATH_API } from './path';
 
 const TIMEOUT_TIME = 10_000;
 
@@ -43,8 +44,35 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => {
-    // error handling
+  async (error) => {
+    // invalid token
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem(REFRESH_TOKEN_KEY!);
+
+      if (refreshToken) {
+        try {
+          const response = await axiosInstance.post(PATH_API.TOKEN_REISSUE, {
+            refreshToken,
+          });
+          const newAccessToken = response.data.accessToken;
+          localStorage.setItem(ACCESS_TOKEN_KEY!, newAccessToken);
+
+          // axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return await axiosInstance(originalRequest);
+        } catch {
+          // 리프레시 토큰도 만료된 경우 로그아웃 처리
+          localStorage.removeItem(ACCESS_TOKEN_KEY!);
+          localStorage.removeItem(REFRESH_TOKEN_KEY!);
+
+          window.location.reload();
+          Promise.resolve('Error! failed token refresh');
+        }
+      }
+    }
+    // timeout
     if (axios.isCancel(error)) {
       // 취소된 요청은 에러로 처리하지 않음
       Promise.resolve();
