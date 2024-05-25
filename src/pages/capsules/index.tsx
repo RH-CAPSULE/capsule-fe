228b9f52942fb2b9495e82d251e9953c39246490
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-25 15:50:38 +0900

    feat: capsule list page

---
+++ src/pages/capsules/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Container } from 'src/components/container';
+
+const CapsulesList = () => {
+  return (
+    <>
+      <Helmet>
+        <title> Capsule List | Capsule</title>
+      </Helmet>
+      <Container>
+        <div>하이</div>
+      </Container>
+    </>
+  );
+};
+
+export default CapsulesList;
