a7a618781cc79d4609c243c574ef87fbad6c2f1b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-25 15:50:59 +0900

    feat: loadable capsules pages

--- src/routes/elements.tsx
+++ src/routes/elements.tsx
@@ -18,6 +18,12 @@ export const LoginPage = Loadable(lazy(() => import('../pages/login')));
 export const SignUpPage = Loadable(lazy(() => import('../pages/sign-up')));
 export const HomePage = Loadable(lazy(() => import('../pages/home')));
 export const HistoryPage = Loadable(lazy(() => import('../pages/history')));
+export const CapsuleListPage = Loadable(
+  lazy(() => import('../pages/capsules'))
+);
+export const CapsuleDetailPage = Loadable(
+  lazy(() => import('../pages/capsules/detail'))
+);
 
 // export const Page500 = Loadable(lazy(() => import('../pages/Page500')));
 // export const Page403 = Loadable(lazy(() => import('../pages/Page403')));
