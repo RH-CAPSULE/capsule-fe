79d83cffc3ea9fd766f52c9a94050dec1ccd98d2
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-26 14:21:20 +0900

    feat: capsule detail section

---
+++ src/apis/queries/capsule/capsule-detail.ts
@@ -0,0 +1,30 @@
+import {
+  UseQueryOptions,
+  UseQueryResult,
+  useQuery,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { PATH_API } from '../../path';
+import { axiosInstance } from '../../axios';
+import { QUERY_KEY } from '../../queryKeys';
+
+type Props = {
+  capsuleId?: string;
+} & Omit<UseQueryOptions, 'queryKey'>;
+
+export const useCapsuleDetail = <T>({ capsuleId, ...other }: Props) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.CAPSULE_DETAIL],
+    queryFn: async () => {
+      const response = await axiosInstance.get(
+        `${PATH_API.CAPSULE_DETAIL}/${capsuleId}`
+      );
+      return response.data;
+    },
+    // 계속 가지고 있을 거임
+    gcTime: Infinity,
+    staleTime: Infinity,
+    enabled: !!capsuleId,
+    ...other,
+  }) as UseQueryResult<T, AxiosError>;
+};
