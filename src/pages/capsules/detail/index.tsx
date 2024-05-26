e09bba9355c716f0d6ac0ffdf879f79e2647a10c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-26 14:21:57 +0900

    feat: Capsule detail page

--- src/pages/capsules/detail/index.tsx
+++ src/pages/capsules/detail/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Container } from 'src/components/container';
+import { BackHeader } from 'src/layouts/header';
+import { CapsuleDetailSection } from 'src/sections/capsule-list';
 
 const CapsuleDetail = () => {
-  return <div>CapsuleDetail</div>;
+  return (
+    <>
+      <Helmet>
+        <title> Capsule Detail | Capsule</title>
+      </Helmet>
+      <Container>
+        <BackHeader />
+        <CapsuleDetailSection />
+      </Container>
+    </>
+  );
 };
 
 export default CapsuleDetail;
