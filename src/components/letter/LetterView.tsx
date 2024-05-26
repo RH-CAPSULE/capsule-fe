8a3b08056c8746a39764f7d30132871aee19f636
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-26 21:54:32 +0900

    feat: Letter View component

--- src/components/letter/LetterView.tsx
+++ src/components/letter/LetterView.tsx
@@ -1,116 +1,55 @@
 import React from 'react';
-import {
-  IconClose,
-  IconImagePlus,
-  IconImagePlusAqua,
-  IconMike,
-  IconMikeAqua,
-  IconPlay,
-  IconStop,
-} from 'src/assets/icons';
-import { RHFInput, RHFTextArea } from '../hook-form';
-// style
+import { RHFInput, RHFTextArea } from 'src/components/hook-form';
+import { useFormContext } from 'react-hook-form';
+import { ICapsule } from 'src/types';
 import styles from './styles.module.scss';
-// component
-import { IconButton } from '../button';
-import ImageUpload from '../image-upload/ImageUpload';
-// type
-import { LetterType, Letters } from '../../types/letter';
-import { Theme } from '../../types';
-import { useAudio } from '../../hooks/useAudio';
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {
-  type?: LetterType;
-  fileInputRef: React.RefObject<HTMLInputElement>;
-  mediaRecorderRef: React.RefObject<MediaRecorder | null>;
-  audioChunks: Blob[];
-  setAudioChunks: React.Dispatch<React.SetStateAction<Blob[]>>;
-  className?: string;
-  methods?: any;
-}
+const LetterView = () => {
+  const { watch } = useFormContext<ICapsule>();
 
-const LetterView = ({
-  type = Letters.PRIMARY,
-  fileInputRef,
-  mediaRecorderRef,
-  audioChunks,
-  setAudioChunks,
-  className,
-  methods,
-  ...other
-}: Props) => {
-  const classes = React.useCallback(() => {
-    const classArr = [styles.container, styles[type]];
-    if (className) classArr.push(className);
+  const { theme, audioUrl, imageUrl } = watch();
+
+  const classes = React.useMemo(() => {
+    const classArr = [styles.container, styles[theme]];
     return classArr.join(' ');
-  }, [type, className]);
-
-  const handleButtonClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const {
-    isPlaying,
-    recording,
-    audioDuration,
-    handleRecordButtonClick,
-    handlePlaybackButtonClick,
-    handleStopButtonClick,
-    handleDeleteAudio,
-  } = useAudio({ mediaRecorderRef, audioChunks, setAudioChunks });
+  }, [theme]);
 
   return (
-    <div className={classes()}>
+    <div className={classes}>
       <div className={`${styles.top} ${styles.toFrom}`}>
         TO.
         <RHFInput name="title" placeholder="캡슐에게.." />
       </div>
-      <ImageUpload fileInputRef={fileInputRef} />
-
-      <div className={styles.contents}>
-        <div className={styles.audio}>
-          {recording && (
-            <IconButton
-              label="녹음 중지"
-              onClick={handleStopButtonClick}
-              theme="AQUA-gray"
-              prevIcon={IconStop}
-              full
-            />
-          )}
-          {audioChunks.length > 0 && (
-            <>
-              <IconButton
-                label={`재생 (${audioDuration})`}
-                onClick={handlePlaybackButtonClick}
-                theme="AQUA-gray"
-                prevIcon={IconPlay}
-                full
-                disabled={isPlaying}
-              />
-              <IconClose onClick={handleDeleteAudio} />
-            </>
-          )}
+      {/* img */}
+      {imageUrl && (
+        <div className={styles.img}>
+          <img src={imageUrl} alt="" />
         </div>
+      )}
+
+      {/* audio */}
+      <div className={styles.contents}>
+        {audioUrl && (
+          <div className={styles.audio}>
+            <audio controls>
+              <source src={audioUrl} type="audio/ogg" />
+              <source src={audioUrl} type="audio/mpeg" />
+              <track
+                src={audioUrl}
+                kind="captions"
+                srcLang="en"
+                label="english_captions"
+              />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        )}
         <div className={styles.textarea}>
           <RHFTextArea name="content" placeholder="내용을 입력해주세요." />
         </div>
       </div>
 
       <div className={styles.bottom}>
-        <IconButton
-          label="이미지 첨부"
-          theme={type === 'PRIMARY' ? '' : Theme.AQUA}
-          className="image"
-          prevIcon={type === 'PRIMARY' ? IconImagePlusAqua : IconImagePlus}
-          onClick={handleButtonClick}
-        />
-        <IconButton
-          theme={type === 'PRIMARY' ? '' : Theme.AQUA}
-          className="image"
-          prevIcon={type === 'PRIMARY' ? IconMikeAqua : IconMike}
-          onClick={handleRecordButtonClick}
-        />
         <div className={`${styles.toFrom} ${styles.right}`}>
           From.
           <RHFInput name="writer" placeholder="캡슐이가" />
