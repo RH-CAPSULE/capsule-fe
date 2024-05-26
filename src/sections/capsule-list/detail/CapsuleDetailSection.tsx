52a1cb4655ed7f1e9f24a1026e6d544b928633ad
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-26 21:54:48 +0900

    feat: capsule detail section

--- src/sections/capsule-list/detail/CapsuleDetailSection.tsx
+++ src/sections/capsule-list/detail/CapsuleDetailSection.tsx
@@ -1,11 +1,11 @@
 import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
-import { FormProvider } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import { useCapsuleDetail } from 'src/apis/queries/capsule';
 import { QUERY_KEY } from 'src/apis/queryKeys';
-import { Letter as LetterComponent } from 'src/components/letter';
-import { useFormData } from 'src/hooks/useFormData';
+import { FormProvider } from 'src/components/hook-form';
+import { LetterView } from 'src/components/letter';
 import { ICapsule } from 'src/types';
 
 interface ResponseType {
@@ -44,18 +44,15 @@ const CapsuleDetailSection = () => {
     enabled: !!capsuleId || !!cachedCapsule,
   });
 
-  const capsule = data || cachedCapsule;
+  const capsule = cachedCapsule || data;
 
-  // 고쳐야댐
-  const fileInputRef = React.useRef<HTMLInputElement>(null);
-  const mediaRecorderRef = React.useRef<MediaRecorder | null>(null);
-  const [audioChunks, setAudioChunks] = React.useState<Blob[]>([]);
+  const methods = useForm({ defaultValues: capsule || {} });
 
-  const { onSubmit, onInvalid, handleSubmit, methods } = useFormData({
-    type: capsule?.theme || 'PRIMARY',
-    fileInputRef,
-    audioChunks,
-  });
+  React.useEffect(() => {
+    if (capsule) {
+      methods.reset(capsule);
+    }
+  }, [capsule, methods]);
 
   if (!capsule) {
     return null;
@@ -63,16 +60,8 @@ const CapsuleDetailSection = () => {
 
   return (
     <div>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <LetterComponent
-            type={capsule.theme}
-            fileInputRef={fileInputRef}
-            mediaRecorderRef={mediaRecorderRef}
-            audioChunks={audioChunks}
-            setAudioChunks={setAudioChunks}
-          />
-        </form>
+      <FormProvider methods={methods}>
+        <LetterView />
       </FormProvider>
     </div>
   );
