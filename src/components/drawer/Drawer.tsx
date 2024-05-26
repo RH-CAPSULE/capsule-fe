28b89c22d651d2f4af8f28001af3ba191464d262
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-05-26 21:53:12 +0900

    feat: add drawer direction

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -5,18 +5,25 @@ import styles from './styles.module.scss';
 const drawerRoot = document.getElementById('drawer-root')!;
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: 'left' | 'right' | 'top' | 'bottom';
   children: React.ReactNode;
   open: boolean;
   onClose: () => void;
 }
 
-const Drawer = ({ children, open, onClose, ...other }: Props) => {
+const Drawer = ({
+  direction = 'right',
+  children,
+  open,
+  onClose,
+  ...other
+}: Props) => {
   const bgRef = React.useRef<HTMLDivElement>(null);
   const drawerRef = React.useRef<HTMLDivElement>(null);
 
   const onBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === bgRef.current) {
-      drawerRef.current!.style.animation = `${styles.rightOut} 0.2s ease forwards`;
+      drawerRef.current!.style.animation = `${styles[`${direction}Out`]} 0.2s ease forwards`;
       drawerRef.current!.onanimationend = () => onClose();
     }
   };
@@ -38,7 +45,11 @@ const Drawer = ({ children, open, onClose, ...other }: Props) => {
       onClick={onBackgroundClick}
       className={styles.drawerContainer}
     >
-      <div ref={drawerRef} className={styles.drawerBox} {...other}>
+      <div
+        ref={drawerRef}
+        className={`${styles.drawerBox} ${styles[direction]}`}
+        {...other}
+      >
         {children}
       </div>
     </div>,
