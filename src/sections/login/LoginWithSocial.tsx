3931c81a3c0da3302b9e89ad302c63937f43bd95
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-06-16 22:28:31 +0900

    feat: oauth sign in handler

--- src/sections/login/LoginWithSocial.tsx
+++ src/sections/login/LoginWithSocial.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
 import { IconGoogle } from 'src/assets/icons';
 import { Button } from 'src/components/button';
+import { axiosInstance } from 'src/apis/axios';
 import styles from './styles.module.scss';
 
 const LoginWithSocial = () => {
+  const handleLogin = async () => {
+    const provider = 'google';
+
+    const urlResponse = await axiosInstance.get(
+      `/oauth/${provider}/sign-in-uri`,
+      {
+        params: {
+          'redirect-uri': `${window.location.origin}/home`,
+        },
+      }
+    );
+    const { signInUri } = urlResponse.data;
+
+    // google sign in
+    window.location.href = signInUri;
+  };
+
   return (
     <section className={styles.section}>
-      <Button className={styles.socialLoginButton}>
+      <Button className={styles.socialLoginButton} onClick={handleLogin}>
         <IconGoogle />
         Sign in with Google
       </Button>
