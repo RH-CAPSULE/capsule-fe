f9bc8310a93134e4aa59064bd6ea662a0d3896b8
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-06-17 23:57:25 +0900

    fix: loading suspense fade out 추가

--- src/components/loading-suspense/index.tsx
+++ src/components/loading-suspense/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoadingScreen from 'src/pages/splash';
+import LoadingEnd from './LoadingEnd';
 
 interface Props {
   isLoading: boolean;
@@ -7,8 +7,27 @@ interface Props {
 }
 
 const LoadingSuspense = ({ isLoading, children }: Props) => {
-  if (isLoading) {
-    return <LoadingScreen />;
+  const [minimumLoadingTimePassed, setMinimumLoadingTimePassed] =
+    React.useState(false);
+
+  React.useEffect(() => {
+    // isLoading이 true일 때만 실행
+    if (!isLoading) {
+      setMinimumLoadingTimePassed(true);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setMinimumLoadingTimePassed(true);
+    }, 1000); // 최소 1초간 스플래시 띄움
+
+    return () => clearTimeout(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading || !minimumLoadingTimePassed) {
+    // 로딩 중이거나 최소 로딩 시간이 지나지 않은 경우
+    return <LoadingEnd />;
   }
 
   return <> {children} </>;
